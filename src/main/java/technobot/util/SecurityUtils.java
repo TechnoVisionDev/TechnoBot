be21f2b2da32925a811eba59024a7f2f53ec6a69
Backendt <matisse.beckandt@gmail.com>
2022-06-06 23:11:22 +0200

    Fixed SSRF by adding a whitelist

---
+++ src/main/java/technobot/util/SecurityUtils.java
@@ -0,0 +1,50 @@
+package technobot.util;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+public class SecurityUtils {
+
+    private static final List<String> ALLOWED_PROTOCOLS = List.of("http", "https");
+    private static final List<String> ALLOWED_DOMAINS = List.of("youtube", "soundcloud", "twitch");
+
+    /**
+     * Check if the given url is a whitelisted domain and protocol.
+     * @param urlString The url to check. Can be malformed/invalid
+     * @return True if the url given is whitelisted
+     */
+    public static boolean isUrlWhitelisted(String urlString) throws MalformedURLException {
+        URL url = new URL(urlString);
+
+        boolean isValidProtocol = ALLOWED_PROTOCOLS.contains(url.getProtocol());
+
+        String host = url.getHost();
+        if(host.equals("youtu.be")) {
+            return true;
+        }
+
+        String domain = getDomain(host);
+        boolean isValidDomain = ALLOWED_DOMAINS.contains(domain);
+
+        return isValidProtocol && isValidDomain;
+    }
+
+    /**
+     * Return the domain of the given host
+     * @param host The host url. Ex: www.youtube.com
+     * @return The domain name without subdomains. Ex: youtube
+     */
+    private static String getDomain(String host) {
+
+        String[] parts = host.split("\\.");
+        int size = parts.length;
+
+        if(size == 1) {
+            return host;
+        }
+
+        return parts[size - 2];
+    }
+
+}
